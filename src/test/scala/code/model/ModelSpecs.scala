6d31339baf3c98cbf6c5ffa43f0ed6f21c3000f4
Mike Rivera <rivera.mj@gmail.com>
2013-04-22 02:12:09 -0400

    created user, updated model spec, added create company service with create, find, get, and delete

--- src/test/scala/code/model/ModelSpecs.scala
+++ src/test/scala/code/model/ModelSpecs.scala
@@ -18,24 +18,49 @@ class ModelSpecs extends Specification {
     "create test company" in {
       val company = Company.create
       company.companyName("company name")
+      company.companyId("identifier")
       company.save must beTrue
     }
     "find a company by name" in {
       val company = Company.find(By(Company.companyName, "company name"))
       company must not beEmpty
-      val name = company.map(company => company.companyName) openOr ""
+
+      val name = company.map(_.companyName) openOr ""
       name must beEqualTo("company name")
     }
     "delete test company" in {
       val company = Company.find(By(Company.companyName, "company name"))
       company must not beEmpty
 
-      company.map(c => c.delete_!).openOr(false) must beTrue
+      company.map(_.delete_!).openOr(false) must beTrue
     }
+  }
 
+  "the User mapper" should {
+    args(sequential=true)
+
+    "create test user" in {
+      val user = User.create
+      user.firstName("Mike")
+      user.lastName("Rivera")
+      user.save must beTrue
+    }
+    "find a user by name" in {
+      val user = User.find(By(User.firstName, "Mike"))
+      user must not beEmpty
+      val name = user.map(_.firstName) openOr ""
+      name must beEqualTo("Mike")
+    }
+    "delete test user" in {
+      val user = User.find(By(User.firstName, "Mike"))
+      user must not beEmpty
+
+      user.map(_.delete_!).openOr(false) must beTrue
+    }
   }
 
   "the Survey mapper" should {
+    args(sequential=true)
     "create test survey" in {
       val survey = Survey.create
       survey.surveyName("survey name")
@@ -44,18 +69,19 @@ class ModelSpecs extends Specification {
     "find a survey by name" in {
       val survey = Survey.find(By(Survey.surveyName, "survey name"))
       survey must not beEmpty
-      val name = survey.map(survey => survey.surveyName) openOr ""
+      val name = survey.map(_.surveyName) openOr ""
       name must beEqualTo("survey name")
     }
     "delete survey company" in {
       val survey = Survey.find(By(Survey.surveyName, "survey name"))
       survey must not beEmpty
 
-      survey.map(s => s.delete_!).openOr(false) must beTrue
+      survey.map(_.delete_!).openOr(false) must beTrue
     }
   }
 
   "the Question mapper" should {
+    args(sequential=true)
     "create test question" in {
       val question = Question.create
       question.question("question 1")
@@ -65,18 +91,19 @@ class ModelSpecs extends Specification {
       val question = Question.find(By(Question.question, "question 1"))
       question must not beEmpty
 
-      val question1 = question.map(question => question.question) openOr ""
+      val question1 = question.map(_.question) openOr ""
       question1 must beEqualTo("question 1")
     }
     "delete test question" in {
       val question = Question.find(By(Question.question, "question 1"))
       question must not beEmpty
 
-      question.map(q => q.delete_!).openOr(false) must beTrue
+      question.map(_.delete_!).openOr(false) must beTrue
     }
   }
 
   "the Answer mapper" should {
+    args(sequential=true)
     "create test answer" in {
       val answer = Answer.create
       answer.answer("answer 1")
@@ -85,14 +112,14 @@ class ModelSpecs extends Specification {
     "find a answer by answer text" in {
       val answer = Answer.find(By(Answer.answer, "answer 1"))
       answer must not beEmpty
-      val answer1 = answer.map(answer => answer.answer) openOr ""
+      val answer1 = answer.map(_.answer) openOr ""
       answer1 must beEqualTo("answer 1")
     }
     "delete test answer" in {
       val answer = Answer.find(By(Answer.answer, "answer 1"))
       answer must not beEmpty
 
-      answer.map(c => c.delete_!).openOr(false) must beTrue
+      answer.map(_.delete_!).openOr(false) must beTrue
     }
   }
 }
