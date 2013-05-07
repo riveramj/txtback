0147de7f4bcb7cce4a451a16d57e5f23ed6c9cb9
Mike Rivera <rivera.mj@gmail.com>
2013-05-07 01:11:53 -0400

    added test data loader v1

---
+++ src/main/scala/com/riveramj/util/TestDataLoader.scala
@@ -0,0 +1,35 @@
+package com.riveramj.util
+
+import net.liftweb.common.Loggable
+import bootstrap.liftweb.Boot
+import com.riveramj.service.CompanyService._
+import com.riveramj.service.UserService
+
+object TestDataLoader extends Loggable {
+
+  def initialize = {
+    val boot = new Boot
+    boot.boot
+  }
+
+
+
+  def main(args: Array[String]) {
+    initialize
+    val testData = new TestDataLoader
+    testData.populateTestUsers()
+  }
+}
+
+class  TestDataLoader extends  Loggable {
+  def populateTestUsers() {
+    val company = createCompany("Company1")
+
+    val companyId = company.map(comp => comp.companyId.get) getOrElse 0L
+    populateTestUser("Mike", "Rivera", "rivera.mj@gmail.com", companyId , "password")
+  }
+
+  private def populateTestUser(firstName: String, lastName: String, email: String, companyId: Long, password: String) {
+    UserService.createUser(email, firstName, lastName, companyId, password)
+  }
+}
