9e730bee158064883eb59682099b0fe450172bd6
Mike Rivera <rivera.mj@gmail.com>
2013-05-15 22:27:49 -0400

    create survey in app

--- src/main/scala/com/riveramj/snippet/home.scala
+++ src/main/scala/com/riveramj/snippet/home.scala
@@ -5,6 +5,7 @@ import net.liftweb.util.Helpers._
 import com.riveramj.util.SecurityContext
 import com.riveramj.service.SurveyService
 import net.liftweb.util.ClearClearable
+import net.liftweb.http.SHtml
 
 object Home {
   val menu = Menu.i("Home") / "Home"
@@ -14,15 +15,23 @@ class Home {
 
   def render() = {
 
+    var surveyName = ""
+
     val currentCompany = SecurityContext.currentCompany
-    val currentCompanyId = SecurityContext.currentCompanyId
-    val surveys = SurveyService.getAllSurveysByCompanyId(currentCompanyId openOr 0)
+    val currentCompanyId = SecurityContext.currentCompanyId.openOr(0L)
+    val surveys = SurveyService.getAllSurveysByCompanyId(currentCompanyId)
+
+    def process() = {
+      SurveyService.createSurvey(surveyName,currentCompanyId)
+    }
 
     ClearClearable andThen
     "#company-name *" #> currentCompany.map(_.companyName.get) &
-    "#survey-list" #> surveys.map{ survey =>
-      ".survey *" #> survey.surveyName.get &
-      ".survey [href]" #> ("/survey/" + survey.surveyId.get)
-    }
+    ".survey" #> surveys.map{ survey =>
+      ".survey a *" #> survey.surveyName.get &
+      ".survey a [href]" #> ("/survey/" + survey.surveyId.get)
+    } &
+    "#survey-name" #> SHtml.text(surveyName,surveyName = _) &
+    "#create-survey" #> SHtml.onSubmitUnit(process _)
   }
 }
