0857e9e9d711ee4fc6258e95d823024969ecb154
Mike Rivera <rivera.mj@gmail.com>
2013-05-15 23:41:25 -0400

    can add and delete questions. can also delete answers. add answers is sorta busted

--- src/main/scala/com/riveramj/snippet/SurveySnippet.scala
+++ src/main/scala/com/riveramj/snippet/SurveySnippet.scala
@@ -5,11 +5,12 @@ import com.riveramj.service.{AnswerService, SurveyService, QuestionService}
 import net.liftweb.util.ClearClearable
 import net.liftweb.util.Helpers._
 import net.liftweb.sitemap._
-import net.liftweb.common.Full
+import net.liftweb.common.{Loggable, Full}
 import net.liftweb.sitemap.Loc.TemplateBox
-import net.liftweb.http.Templates
+import net.liftweb.http.{SHtml, Templates}
 import com.riveramj.util.Paths
 import com.riveramj.model.Question
+import net.liftweb.http.js.{JsCmds, JsCmd}
 
 
 object SurveySnippet {
@@ -20,16 +21,52 @@ object SurveySnippet {
   TemplateBox(() => Templates( "survey" :: Nil))
 }
 
-class SurveySnippet {
+class SurveySnippet extends Loggable {
   import SurveySnippet._
+
+  var newAnswer = ""
+  var newQuestion = ""
+
+  def deleteQuestion(questionId: Long):JsCmd = {
+    QuestionService.deleteQuestionById(questionId) match {
+      case Full(true) =>
+        JsCmds.Run("$('#" + questionId + "').parent().remove()")
+      case _ => logger.error("couldn't delete survey with id %s" format questionId)
+    }
+
+  }
+
+  def deleteAnswer(answerId: Long):JsCmd = {
+    AnswerService.deleteAnswerById(answerId) match {
+      case Full(true) =>
+        JsCmds.Run("$('#" + answerId + "').parent().remove()")
+      case _ => logger.error("couldn't delete survey with id %s" format answerId)
+    }
+
+  }
+
+  def createAnswer(questionId: Long) = {
+    AnswerService.createAnswer(List(newAnswer), questionId)
+  }
+
+  def createQuestion(surveyId: Long) = {
+    QuestionService.createQuestion(newQuestion, surveyId)
+  }
 
   def questionAndAnswers(question: Question) = {
     val answers = AnswerService.findAllAnswersByQuestionId(question.questionId.get)
-    ".question *" #> question.question.get &
-    "#answer-list" #> answers.map{ answer =>
-      ".answer *" #> answer.answer.get
-    }
+    var answer = ""
 
+    ".question *" #> question.question.get &
+    ".question [id]" #> question.questionId.get &
+    ".delete-question [onclick]" #> SHtml.ajaxInvoke(() => deleteQuestion(question.questionId.get)) &
+    ".answer" #> answers.map{ answer =>
+      "span *" #> answer.answer.get &
+      "span [id]" #> answer.answerId.get &
+      ".delete-answer [onclick]" #> SHtml.ajaxInvoke(() => deleteAnswer(answer.answerId.get))
+    } &
+    "#new-answer" #> SHtml.text("", answer = _) &
+    "#create-answer" #> SHtml.onSubmitUnit(() => createAnswer(question.questionId.get))
   }
 
   def render() = {
@@ -40,9 +77,11 @@ class SurveySnippet {
     val questions = QuestionService.findAllSurveyQuestions(surveyId)
 
     ClearClearable andThen
-      "#survey-name *" #> survey.map(_.surveyName.get) &
-      "#question-list" #> questions.map{ question =>
-          questionAndAnswers(question)
-        }
+    "#survey-name *" #> survey.map(_.surveyName.get) &
+    "#question-list" #> questions.map{ question =>
+        questionAndAnswers(question)
+      } &
+    "#new-question" #> SHtml.text(newQuestion,newQuestion = _) &
+    "#create-question" #> SHtml.onSubmitUnit(() => createQuestion(surveyId))
   }
 }
