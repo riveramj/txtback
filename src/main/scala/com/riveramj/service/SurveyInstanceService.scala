c3b3d18ac713c9c2b6609cdb32e0e8ce24f94aec
Mike Rivera <rivera.mj@gmail.com>
2013-05-19 17:49:58 -0400

    Created SurveyInstance service. Renamed respondee to responder. cleaned up build.sbt

---
+++ src/main/scala/com/riveramj/service/SurveyInstanceService.scala
@@ -0,0 +1,63 @@
+package com.riveramj.service
+
+import net.liftweb.common._
+import com.riveramj.model.SurveyInstance
+import net.liftweb.mapper.By
+import org.joda.time.DateTime
+import net.liftweb.util.Helpers._
+
+
+object SurveyInstanceService extends Loggable {
+
+  def createSurveyInstance(responderPhone:String, surveyId:Long) = {
+    val surveyInstance = SurveyInstance.create
+      .responderPhone(responderPhone)
+      .dateStarted(DateTime.now())
+      .SurveyId(surveyId)
+
+    tryo(saveSurveyInstance(surveyInstance)) flatMap {
+      u => u match {
+        case Full(newSurveyInstance:SurveyInstance) => Full(newSurveyInstance)
+        case (failure: Failure) => failure
+        case _ => Failure("Unknown error")
+      }
+    }
+  }
+
+  def saveSurveyInstance(surveyInstance:SurveyInstance):Box[SurveyInstance] = {
+
+    val validateErrors = surveyInstance.validate
+
+    if (validateErrors.isEmpty) {
+      tryo(surveyInstance.saveMe()) match {
+        case Full(newSurveyInstance:SurveyInstance) =>
+          Full(newSurveyInstance)
+        case Failure(_, Full(err), _) =>
+          logger.error("save surveyInstance failed with %s" format err)
+          Failure("Unknown error")
+        case _ =>
+          Failure("Unknown error")
+      }
+    } else {
+      Failure("Validations Failed")
+    }
+  }
+
+  def getSurveyInstanceById(surveyInstanceId: Long): Box[SurveyInstance] = {
+    SurveyInstance.find(By(SurveyInstance.surveyInstanceId, surveyInstanceId))
+  }
+
+  def deleteSurveyInstanceById(surveyInstanceId: Long): Box[Boolean] = {
+    val surveyInstance = SurveyInstance.find(By(SurveyInstance.surveyInstanceId, surveyInstanceId))
+    surveyInstance.map(_.delete_!)
+  }
+
+  def findAllSurveySurveyInstances(surveyInstanceId:Long): List[SurveyInstance] = {
+    SurveyInstance.findAll(By(SurveyInstance.surveyInstanceId, surveyInstanceId))
+  }
+
+  def getAllSurveyInstances = {
+    SurveyInstance.findAll()
+  }
+
+}
