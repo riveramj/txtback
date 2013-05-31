415e3d1d537ed22fa6dd13d05b4e99ffcfc14f09
Mike Rivera <rivera.mj@gmail.com>
2013-05-31 00:41:38 -0400

    heroku sessions2

--- src/main/scala/JettyLauncher.scala
+++ src/main/scala/JettyLauncher.scala
@@ -1,6 +1,7 @@
 import java.util
 import javax.servlet.DispatcherType
 import org.eclipse.jetty.server.Server
+import org.eclipse.jetty.server.session.SessionHandler
 import org.eclipse.jetty.servlet.{FilterHolder, DefaultServlet, ServletContextHandler}
 import org.eclipse.jetty.server.nio.SelectChannelConnector
 import net.liftweb.http.LiftFilter
@@ -14,6 +15,7 @@ object JettyLauncher extends App {
   server.setConnectors(Array(scc))
 
   val context = new ServletContextHandler(server, "/", ServletContextHandler.SESSIONS)
+  context.setSessionHandler(new SessionHandler())
   context.addServlet(classOf[DefaultServlet], "/")
   context.addFilter(classOf[LiftFilter], "/*", util.EnumSet.of(DispatcherType.REQUEST))
   context.setResourceBase("src/main/webapp")
