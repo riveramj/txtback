7f480f878084d8f29dd4e324d457df008b5df050
Mike Rivera <rivera.mj@gmail.com>
2013-05-31 00:27:13 -0400

    more heroku changes

--- src/main/scala/JettyLauncher.scala
+++ src/main/scala/JettyLauncher.scala
@@ -1,10 +1,12 @@
+import java.util
+import javax.servlet.DispatcherType
 import org.eclipse.jetty.server.Server
-import org.eclipse.jetty.servlet.{DefaultServlet, ServletContextHandler}
+import org.eclipse.jetty.servlet.{FilterHolder, DefaultServlet, ServletContextHandler}
 import org.eclipse.jetty.server.nio.SelectChannelConnector
 import net.liftweb.http.LiftFilter
 
 
-object JettyLauncher extends App {
+object JettyLauncher extends Application {
   val port = if(System.getenv("PORT") != null) System.getenv("PORT").toInt else 8080
   val server = new Server
   val scc = new SelectChannelConnector
@@ -13,6 +15,7 @@ object JettyLauncher extends App {
 
   val context = new ServletContextHandler(server, "/", ServletContextHandler.NO_SESSIONS)
   context.addServlet(classOf[DefaultServlet], "/")
+  context.addFilter(classOf[LiftFilter], "/*", util.EnumSet.of(DispatcherType.REQUEST))
   context.setResourceBase("src/main/webapp")
 
   server.start
