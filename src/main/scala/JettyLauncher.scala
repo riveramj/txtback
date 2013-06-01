786650d4e1ff4be22351506da2d328edb9b577be
Mike Rivera <rivera.mj@gmail.com>
2013-06-01 19:27:48 -0400

    changes for run mode

--- src/main/scala/JettyLauncher.scala
+++ src/main/scala/JettyLauncher.scala
@@ -1,26 +1,43 @@
 import java.util
 import javax.servlet.DispatcherType
 import org.eclipse.jetty.server.Server
-import org.eclipse.jetty.server.session.SessionHandler
-import org.eclipse.jetty.servlet.{FilterHolder, DefaultServlet, ServletContextHandler}
+import org.eclipse.jetty.server.session.{SessionHandler, HashSessionManager}
+import org.eclipse.jetty.servlet.{DefaultServlet, ServletContextHandler}
 import org.eclipse.jetty.server.nio.SelectChannelConnector
 import net.liftweb.http.LiftFilter
 
+object JettyLauncher {
 
-object JettyLauncher extends App {
-  val port = if(System.getenv("PORT") != null) System.getenv("PORT").toInt else 8080
-  val server = new Server
-  val scc = new SelectChannelConnector
-  scc.setPort(port)
-  server.setConnectors(Array(scc))
+  def main(args: Array[String]) {
 
-  val context = new ServletContextHandler(server, "/", ServletContextHandler.SESSIONS)
-  context.setSessionHandler(new SessionHandler())
-  context.addServlet(classOf[DefaultServlet], "/")
-  context.addFilter(classOf[LiftFilter], "/*", util.EnumSet.of(DispatcherType.REQUEST))
-  context.setResourceBase("src/main/webapp")
+    //Transfer the run.mode from system environment to system properties. Otherwise Lift's calculation of
+    //mode will not work
+    if (System.getProperty("run.mode") == null){
+      System.setProperty("run.mode", System.getenv("run.mode"))
+    }
 
-  server.setHandler(context)
-  server.start
-  server.join
+    //Create the server and set the port to listen to
+    val port = if (System.getenv("PORT") != null) System.getenv("PORT").toInt else 8080
+    val server = new Server
+    val scc = new SelectChannelConnector
+    scc.setPort(port)
+    server.setConnectors(Array(scc))
+
+    //Initialize the session handler
+    val sessionHandler = new SessionHandler()
+    val sessionManager = new HashSessionManager()
+    sessionHandler.setSessionManager(sessionManager)
+    sessionManager.setMaxInactiveInterval(600)
+
+    val context = new ServletContextHandler(server, "/", ServletContextHandler.SESSIONS)
+    context.setSessionHandler(sessionHandler)
+    context.addServlet(classOf[DefaultServlet], "/")
+    context.addFilter(classOf[LiftFilter], "/*", util.EnumSet.of(DispatcherType.REQUEST))
+    context.setResourceBase("src/main/webapp")
+
+    server.start
+    server.join
+
+  }
+
 }
