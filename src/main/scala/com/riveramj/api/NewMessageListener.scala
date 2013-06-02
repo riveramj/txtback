cc7466015512795abc00eba9ca80f6443b87f6df
Mike Rivera <rivera.mj@gmail.com>
2013-06-01 22:37:29 -0400

    new message post take 1

---
+++ src/main/scala/com/riveramj/api/NewMessageListener.scala
@@ -0,0 +1,19 @@
+package com.riveramj.api
+
+import net.liftweb.http._
+import net.liftweb.http.rest._
+import net.liftweb.common.{Loggable, Full}
+import net.liftweb.json.JsonAST.{JString, JValue}
+
+object NewMessageListener extends RestHelper with Loggable {
+
+  serve("api" / "textMessage" prefix {
+    case "newMessage"  :: _ Post req =>
+    {
+      println("======= " + req)
+      JString("ok")
+
+    }
+  })
+
+}
