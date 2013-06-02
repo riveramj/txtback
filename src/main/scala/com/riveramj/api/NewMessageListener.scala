4c518ed469ee3b268a511373ba1c9d1789d31f38
Mike Rivera <rivera.mj@gmail.com>
2013-06-01 23:56:03 -0400

    more param work

--- src/main/scala/com/riveramj/api/NewMessageListener.scala
+++ src/main/scala/com/riveramj/api/NewMessageListener.scala
@@ -10,7 +10,14 @@ object NewMessageListener extends RestHelper with Loggable {
   serve("api" / "textMessage" prefix {
     case "newMessage"  :: _ Post req =>
     {
-      println("======= " + req._params)
+      println("======= " + req.paramNames)
+      println("======= " + req.param("Body"))
+
+      val fromPhone = req.param("From")
+      val msg = req.param("Body")
+
+      println("body is %s and its from %s" format(msg,fromPhone))
+
       JString("ok")
 
     }
