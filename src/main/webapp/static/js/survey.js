421b143904d94e5e9ecf27f87f1b3945bc281269
Mike Rivera <rivera.mj@gmail.com>
2013-07-28 22:47:32 -0400

    save question edits

--- src/main/webapp/static/js/survey.js
+++ src/main/webapp/static/js/survey.js
@@ -2,6 +2,6 @@
   $(document)
     .on('click', '.edit-question', function(event) {
       $('#reload-page').click();
-      $('#edit-question').modal('show');
+      $('#edit-question').modal('show')
     })
 })();
