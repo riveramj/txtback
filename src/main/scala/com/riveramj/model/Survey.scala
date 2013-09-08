3b5cfb47cda3e51f99f995da30616f8f52e312af
Mike Rivera <rivera.mj@gmail.com>
2013-09-08 16:31:51 -0400

    more conversions to mongo

--- src/main/scala/com/riveramj/model/Survey.scala
+++ src/main/scala/com/riveramj/model/Survey.scala
@@ -3,11 +3,26 @@ package com.riveramj.model
 import org.bson.types.ObjectId
 import net.liftweb.mongodb.{ObjectIdSerializer, MongoDocumentMeta, MongoDocument}
 
+sealed trait QuestionType
+object QuestionType {
+  case object trueFalse extends QuestionType
+  case object ratingScale extends QuestionType
+  case object freeResponse extends QuestionType
+  case object choseOne extends QuestionType
+}
+
 case class Questions(
   _id: ObjectId,
   question: String,
-  questionType: String,
-  questionNumber: Int
+  questionType: QuestionType,
+  questionNumber: Int,
+  answers: List[Answers]
+)
+
+case class Answers(
+  _id: ObjectId,
+  answerNumber: Int,
+  answer: String
 )
 
 case class Survey(
