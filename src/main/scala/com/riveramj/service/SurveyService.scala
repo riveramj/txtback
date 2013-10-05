8e3ff0f56ef82040a5208659edd8835610a601f2
Mike Rivera <rivera.mj@gmail.com>
2013-10-05 12:58:28 -0400

    change, delete, and save answers works again

--- src/main/scala/com/riveramj/service/SurveyService.scala
+++ src/main/scala/com/riveramj/service/SurveyService.scala
@@ -27,15 +27,12 @@ object SurveyService extends Loggable {
   }
 
   def updateSurvey(query: JObject, survey: Survey) = {
-    println(survey + " one to be inserted")
-    println(Survey.find("_id" -> ("$oid" -> survey._id.toString)) + " +_+_+_+_+_+_ in update survey")
-
+    println(survey + " one to be inserted111111")
     Survey.update(query, survey)
   }
 
   def updateSurvey(survey: Survey) = {
-    println(survey + " one to be inserted")
-    println(Survey.find("_id" -> ("$oid" -> survey._id.toString)) + " +_+_+_+_+_+_ in update survey")
+    println(survey + " one to be inserted222222")
     Survey.update("_id" -> ("$oid" -> survey._id.toString), survey)
   }
 
