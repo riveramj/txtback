6652fab4ecefce194d86fdd0ad5060463fe754bb
Mike Rivera <rivera.mj@gmail.com>
2013-10-30 23:25:04 -0400

    question length is counted now

--- src/main/webapp/static/js/survey.js
+++ src/main/webapp/static/js/survey.js
@@ -4,4 +4,8 @@
       $('#reload-page').click();
       $('#edit-question').modal('show');
     })
+  $("#new-question").keypress(function() {
+    var length = $("#new-question").val().length + 1
+    $("#question-length").text(length);
+  }); 
 })();
