230be21125f186c81a89238c6433f107f6c2408b
Mike Rivera <rivera.mj@gmail.com>
2013-11-16 21:56:04 -0500

    signup validates fields only so far

--- src/main/scala/com/riveramj/snippet/Signup.scala
+++ src/main/scala/com/riveramj/snippet/Signup.scala
@@ -1,20 +1,49 @@
 package com.riveramj.snippet
 
 import net.liftweb.sitemap.Menu
-import net.liftweb.http.{S, SHtml}
+import net.liftweb.http.{S, SHtml, StatefulSnippet}
 import net.liftweb.util.Helpers._
 import com.riveramj.service.SurveyorService._
 import com.riveramj.model.Surveyor
 import net.liftweb.common._
-import com.riveramj.util.SecurityContext
-import net.liftweb.common.Full
 import net.liftweb.http.js.JsCmds
-import net.liftweb.util.Props
+import com.riveramj.service.ValidationService._
 
 
 object Signup {
   val menu = Menu.i("signup") / "signup"
 }
-class Signup extends Loggable {
-  
+class Signup extends Loggable with StatefulSnippet {
+
+  def dispatch = {case _ => render}
+
+  def render = {
+    var email = ""
+    var password = ""
+    var firstName = ""
+    var lastName = ""
+    var company = ""
+
+    def createUser() = {
+      val validateFields = List(
+        checkEmail(email, "email-error"),
+        checkCompany(company, "company-error"),
+        checkEmpty(firstName, "first-name-error"),
+        checkEmpty(lastName, "last-name-error"),
+        checkEmpty(password, "password-error")
+      ).flatten
+
+      for (error <- validateFields) {
+        S.error(error.id, error.message)
+      }
+    }
+
+    "#first-name" #> SHtml.text(firstName, firstName= _) &
+    "#last-name" #> SHtml.text(lastName, lastName= _) &
+    "#company" #> SHtml.text(company, company= _) &
+    "#email" #> SHtml.text(email, email= _) &
+    "#password" #> SHtml.password(password, password = _) &
+    "#signup" #> SHtml.onSubmitUnit(createUser)
+  }
+
 }
