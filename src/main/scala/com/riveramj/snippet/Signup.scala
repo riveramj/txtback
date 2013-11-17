a693961bc92b36bde2992b2c9b35738584dd7e28
Mike Rivera <rivera.mj@gmail.com>
2013-11-16 23:20:51 -0500

    if validations pass, create company and user

--- src/main/scala/com/riveramj/snippet/Signup.scala
+++ src/main/scala/com/riveramj/snippet/Signup.scala
@@ -4,10 +4,12 @@ import net.liftweb.sitemap.Menu
 import net.liftweb.http.{S, SHtml, StatefulSnippet}
 import net.liftweb.util.Helpers._
 import com.riveramj.service.SurveyorService._
+import com.riveramj.service.CompanyService._
 import com.riveramj.model.Surveyor
 import net.liftweb.common._
 import net.liftweb.http.js.JsCmds
 import com.riveramj.service.ValidationService._
+import com.riveramj.model.Company
 
 
 object Signup {
@@ -22,28 +24,45 @@ class Signup extends Loggable with StatefulSnippet {
     var password = ""
     var firstName = ""
     var lastName = ""
-    var company = ""
+    var companyName = ""
 
     def createUser() = {
       val validateFields = List(
         checkEmail(email, "email-error"),
-        checkCompany(company, "company-error"),
+        checkCompany(companyName, "company-error"),
         checkEmpty(firstName, "first-name-error"),
         checkEmpty(lastName, "last-name-error"),
         checkEmpty(password, "password-error")
       ).flatten
 
-      for (error <- validateFields) {
-        S.error(error.id, error.message)
+      if(validateFields.isEmpty) {
+        val company: Box[Company] = 
+          if(!companyName.isEmpty)
+            createCompany(companyName)
+          else
+            Empty
+
+        val user = createSurveyor(
+          firstName = firstName,
+          lastName = lastName,
+          email = email,
+          password = password,
+          companyId = company.map(_._id)
+        )
+        user
+      }
+      else {
+        for (error <- validateFields) {
+          S.error(error.id, error.message)
+        }
       }
     }
 
-    "#first-name" #> SHtml.text(firstName, firstName= _) &
-    "#last-name" #> SHtml.text(lastName, lastName= _) &
-    "#company" #> SHtml.text(company, company= _) &
-    "#email" #> SHtml.text(email, email= _) &
+    "#first-name" #> SHtml.text(firstName, firstName = _) &
+    "#last-name" #> SHtml.text(lastName, lastName = _) &
+    "#company" #> SHtml.text(companyName, companyName = _) &
+    "#email" #> SHtml.text(email, email = _) &
     "#password" #> SHtml.password(password, password = _) &
     "#signup" #> SHtml.onSubmitUnit(createUser)
   }
-
 }
