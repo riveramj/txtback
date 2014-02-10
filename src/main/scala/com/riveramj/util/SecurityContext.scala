07d184ab0fea4477828a4f20480f0fd8443c203f
Mike Rivera <rivera.mj@gmail.com>
2014-02-10 00:29:08 -0500

    remove comments

--- src/main/scala/com/riveramj/util/SecurityContext.scala
+++ src/main/scala/com/riveramj/util/SecurityContext.scala
@@ -8,14 +8,6 @@ import com.riveramj.service.SurveyorService._
 import com.riveramj.service.CompanyService._
 import org.bson.types.ObjectId
 
-/**
- * Provides the security context information for the current session.
- *
- * All determinations of who the current user is should go through this singleton. When a user
- * logs in, their user ID is stored in the current session, but the actual user object is refreshed
- * with each full-page load. AJAX requests should be in the same request scope and use the same
- * instance of the user without retrieving a new one from Scribe.
- */
 object SecurityContext extends Loggable {
   private object loggedInUserId extends SessionVar[Box[ObjectId]](Empty)
   private object loggedInCompanyId extends SessionVar[Box[ObjectId]](Empty)
@@ -28,9 +20,6 @@ object SecurityContext extends Loggable {
   def logIn(user: Surveyor) {
 
     setCurrentUser(user)
-
-    // We immediately access the request var to suppress the warning about it
-    // never being accessed during a request.
     loggedInUser.is
 
     logger.info("Logged user in [ %s ]".format(user.email ))
@@ -75,8 +64,6 @@ object SecurityContext extends Loggable {
       val user = getUserById(userId)
       loggedInUser(user)
 
-      // We immediately access the request var to suppress the warning about it
-      // never being accessed during a request.
       loggedInUser.is
     }
   }
