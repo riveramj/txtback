f33cf00d561cdfb7694ed8fc6a6d7cbe18f583d6
Mike Rivera <rivera.mj@gmail.com>
2014-04-12 18:31:23 -0400

    populate real results for signup search

--- src/main/scala/com/riveramj/snippet/Signup.scala
+++ src/main/scala/com/riveramj/snippet/Signup.scala
@@ -7,10 +7,11 @@ import com.riveramj.service.SurveyorService._
 import com.riveramj.service.TwilioService
 import com.riveramj.model.Surveyor
 import net.liftweb.common._
+import net.liftweb.util.ClearClearable
 import net.liftweb.http.js.JsCmds
+import net.liftweb.http.IdMemoizeTransform
 import com.riveramj.service.ValidationService._
 import com.riveramj.util.SecurityContext
-
 
 object Signup {
   val menu = Menu.i("signup") / "signup"
@@ -26,6 +27,7 @@ class Signup extends Loggable with StatefulSnippet {
     var lastName = ""
     var areaCode = ""
     var phone = ""
+    var availableNumbers: List[String] = Nil 
     
 
     def signupUser() = {
@@ -60,12 +62,26 @@ class Signup extends Loggable with StatefulSnippet {
       }
     }
 
+    def findPhoneNumbers(renderer: IdMemoizeTransform) = {
+      availableNumbers = TwilioService.lookupPhoneNumbers(areaCode, phone).toList
+      renderer.setHtml
+    }
+
+    ClearClearable andThen
     "#first-name" #> SHtml.text(firstName, firstName = _) &
     "#last-name" #> SHtml.text(lastName, lastName = _) &
     "#email" #> SHtml.text(email, email = _) &
     "#password" #> SHtml.password(password, password = _) &
-    "#area-code" #> SHtml.text(areaCode, areaCode = _) &
-    "#phone" #> SHtml.text(phone, phone = _) &
+    ".available-numbers"  #> SHtml.idMemoize { renderer =>
+      "#area-code" #> SHtml.ajaxText(areaCode, areaCode = _) &
+      "#phone" #> SHtml.ajaxText(phone, phone = _) &
+      ".number-search [onclick]" #> SHtml.ajaxInvoke(() => findPhoneNumbers(renderer)) &
+      ".number-entry" #> availableNumbers.map { availableNumber =>
+        "div [id]" #> availableNumber &
+        "div *" #> availableNumber &
+        "input [value]" #> availableNumber
+      }
+    } &
     ".signup button" #> SHtml.onSubmitUnit(signupUser)
   }
 }
--- src/main/webapp/coffee-script/signup.coffee
+++ src/main/webapp/coffee-script/signup.coffee
@@ -1,7 +1,8 @@
 $(document).ready ->
-  $(".number-entry")
-    .on('click', (event) ->
+  $(".available-numbers")
+    .on('click', ".number-entry", (event) ->
       $('.number-entry').removeClass('selected')
       $(this).addClass('selected')
       $(this).find('input[type="radio"]').prop('checked', true)
+      event.stopPropagation()
     )
