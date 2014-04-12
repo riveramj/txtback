c2ab2037e3540abdd859f22565ae27f06d68baa5
Mike Rivera <rivera.mj@gmail.com>
2014-04-12 11:58:38 -0400

    fix compile warnings

--- src/main/scala/bootstrap/liftweb/MongoConfig.scala
+++ src/main/scala/bootstrap/liftweb/MongoConfig.scala
@@ -1,6 +1,6 @@
 package bootstrap.liftweb
 
-import com.mongodb.{Mongo, ServerAddress}
+import com.mongodb.{Mongo, ServerAddress, MongoClient}
 import net.liftweb.util.Props
 
 import net.liftweb._
@@ -9,10 +9,13 @@ import mongodb._
 object MongoConfig {
 
   def init = {
-    val srvr = new ServerAddress(
+    val address = new ServerAddress(
       Props.get("mongo.host", "127.0.0.1"),
       Props.getInt("mongo.port", 27017)
     )
-    MongoDB.defineDb(DefaultMongoIdentifier, new Mongo(srvr), "txtback")
+    
+    val mongo = new MongoClient(address)
+    
+    MongoDB.defineDb(DefaultMongoIdentifier, mongo, "txtback")
   }
 }
