931ab96971771fbbb572f3ca539969fd6d3a4409
Mike Rivera <rivera.mj@gmail.com>
2014-04-12 00:44:44 -0400

    twilio service cleanup

--- src/main/scala/com/riveramj/service/TwilioService.scala
+++ src/main/scala/com/riveramj/service/TwilioService.scala
@@ -22,11 +22,12 @@ import util.Props
 object TwilioService extends Loggable with WrapAsJava {
   val ACCOUNT_SID = "AC7b257aff635e86c50a87e5755bf0fd79" //TODO: put in props file
   val AUTH_TOKEN = "82dca0784a423ff566696aa495f702b0" //TODO: put in props file
+  val twilioClient = new TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
+  val messageFactory = twilioClient.getAccount.getSmsFactory
+   
   val TEST_ACCOUNT_SID = "AC5864c960b5fd10f1af07317c81dbe17a" //TODO: put in props file
   val TEST_AUTH_TOKEN = "6695764111318a945ee8a89182ac9b7a" //TODO: put in props file
-  val twilioClient = new TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
   val TestTwilioClient = new TwilioRestClient(TEST_ACCOUNT_SID, TEST_AUTH_TOKEN)
-  val messageFactory = twilioClient.getAccount.getSmsFactory
 
   def sendMessage(toPhoneNumber: String, message: String, fromPhoneNumber: String = "7702123225") {
     val params = Map(
@@ -56,13 +57,15 @@ object TwilioService extends Loggable with WrapAsJava {
     )
 
     val availablePhoneNumbers = account.getAvailablePhoneNumbers(mapAsJavaMap(params)).getPageData()
+
+    availablePhoneNumbers.asScala.map(_.getPhoneNumber())
   }
 
   def buyPhoneNumber(phoneNumber: String) = {
     // https://www.twilio.com/docs/howto/search-and-buy
     // test number to buy is +15005550006
     val purchaseParams = Map(
-      "PhoneNumber" -> s"+$phoneNumber"
+      "PhoneNumber" -> s"+1$phoneNumber"
     )
 
     val purchasedNumber = TestTwilioClient.getAccount.getIncomingPhoneNumberFactory().create(mapAsJavaMap(purchaseParams))
