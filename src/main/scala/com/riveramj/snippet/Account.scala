4cbed5efec5852e348755debd84fb827ef8f97a6
Mike Rivera <rivera.mj@gmail.com>
2014-04-18 18:56:03 -0400

    lookup phone numbers on account screen

--- src/main/scala/com/riveramj/snippet/Account.scala
+++ src/main/scala/com/riveramj/snippet/Account.scala
@@ -1,14 +1,21 @@
 package com.riveramj.snippet
 
+import com.riveramj.service.SurveyorService._
+import com.riveramj.service.{TwilioService, PhoneNumberService}
+import com.riveramj.model.Surveyor
+import com.riveramj.service.ValidationService._
+import com.riveramj.util.SecurityContext
+
+import net.liftweb.common._
 import net.liftweb.sitemap.Menu
 import net.liftweb.http.{S, SHtml}
 import net.liftweb.util.Helpers._
-import com.riveramj.service.SurveyorService._
-import com.riveramj.model.Surveyor
-import net.liftweb.common._
+import net.liftweb.util.ClearClearable
 import net.liftweb.http.js.JsCmds
-import com.riveramj.service.ValidationService._
-import com.riveramj.util.SecurityContext
+import net.liftweb.http.IdMemoizeTransform
+
+import scala.xml.NodeSeq
+
 
 object Account {
   import com.riveramj.util.PathHelpers.loggedIn
@@ -21,10 +28,15 @@ class Account extends Loggable {
   val user = SecurityContext.currentUser openOrThrowException "Need Valid User"
 
   def edit = {
-    println(user)
     var email = user.email
     var firstName = user.firstName
     var lastName = user.lastName
+
+    var availableNumbers: List[String] = Nil
+    var phoneNumberRadios: NodeSeq = Nil 
+    var selectedNumber = ""
+    var areaCode = ""
+    var phone = ""
 
     def updateUser() = {
       val emailError = 
@@ -56,9 +68,35 @@ class Account extends Loggable {
       }
     }
 
+    def findPhoneNumbers(renderer: IdMemoizeTransform) = {
+      println(areaCode)
+      println(phone)
+      val rawNumbers = TwilioService.lookupPhoneNumbers(areaCode, phone)
+      availableNumbers = rawNumbers.map { number => 
+        PhoneNumberService.longFormatPhoneNumber(number)
+      }
+
+      phoneNumberRadios = SHtml.radio(availableNumbers.toSeq, Empty, selectedNumber = _).toForm 
+
+      renderer.setHtml
+    }
+
+
+    ClearClearable andThen
     "#first-name" #> SHtml.text(firstName, firstName = _) &
     "#last-name" #> SHtml.text(lastName, lastName = _) &
     "#email" #> SHtml.text(email, email = _) &
+    "#available-numbers" #> user.phoneNumbers.map { number => 
+      ".available-number *" #> number
+    } &
+    ".available-numbers"  #> SHtml.idMemoize { renderer =>
+      "#area-code" #> SHtml.ajaxText(areaCode, areaCode = _) &
+      "#phone" #> SHtml.ajaxText(phone, phone = _) &
+      ".number-search [onclick]" #> SHtml.ajaxInvoke(() => findPhoneNumbers(renderer)) &
+      ".number-entry" #> phoneNumberRadios.map { radio => 
+        "input" #> radio
+      }
+    } &
     ".save-user button" #> SHtml.onSubmitUnit(updateUser)
   }
 }
---
+++ src/main/webapp/coffee-script/account.coffee
@@ -0,0 +1,21 @@
+$(document).ready ->
+  $(".available-numbers")
+    .on('click', ".number-entry", (event) ->
+      $('.number-entry').removeClass('selected')
+      $(this).addClass('selected')
+      $(this).find('input[type="radio"]').prop('checked', true)
+      event.stopPropagation()
+    )
+    .on('keydown', "#phone", (event) ->
+      if event.which == 13
+        this.blur()
+        $(".number-search").click()
+        event.preventDefault()
+    )
+    .on('keydown', "#area-code", (event) ->
+      if event.which == 13
+        this.blur()
+        $(".number-search").click()
+        event.preventDefault()
+    )
+   
