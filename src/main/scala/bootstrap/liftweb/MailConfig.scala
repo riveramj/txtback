296cd2f7e200e3ac869e5b25c0b3f61e486f6d5f
Mike Rivera <rivera.mj@gmail.com>
2014-04-22 23:13:00 -0400

    print email to console in dev mode

--- src/main/scala/bootstrap/liftweb/MailConfig.scala
+++ src/main/scala/bootstrap/liftweb/MailConfig.scala
@@ -4,17 +4,64 @@ import net.liftweb.util.Props
 import net.liftweb.common._
 import net.liftweb.util._
 import javax.mail.{Authenticator, PasswordAuthentication}
+import javax.mail.internet.{MimeMessage, MimeMultipart}
 
-object MailConfig {
+object MailConfig extends Loggable {
 
   def init = {
-    (Props.get("mail.user"), Props.get("mail.password")) match {
-      case (Full(username), Full(password)) =>
-      Mailer.authenticator = Full(new Authenticator() {
-        override def getPasswordAuthentication = new
-        PasswordAuthentication(username, password)
-      })
-      case _ => throw new Exception("Username/password not supplied for Mailer.")
+    if (Props.mode == Props.RunModes.Development) {
+      Mailer.devModeSend.default.set((m: MimeMessage) => logger.info("Dev mode message:\n" + prettyPrintMime(m)))
+    } else {
+      (Props.get("mail.user"), Props.get("mail.password")) match {
+        case (Full(username), Full(password)) =>
+        Mailer.authenticator = Full(new Authenticator() {
+          override def getPasswordAuthentication = new
+          PasswordAuthentication(username, password)
+        })
+        case _ => throw new Exception("Username/password not supplied for Mailer.")
+      }
     }
   }
+
+  private def prettyPrintMime(mimeMessage: MimeMessage): String = {
+    val headerBuilder = new StringBuilder
+    val headers = mimeMessage.getAllHeaderLines
+
+    while (headers.hasMoreElements)
+      headerBuilder ++= headers.nextElement.toString + "\n"
+
+    def content(message: MimeMessage): String = {
+      message.getContent match {
+        case multipart: MimeMultipart => {
+          for {
+            index <- 0 until multipart.getCount
+            bodyPart = multipart.getBodyPart(index)
+          } yield {
+            val partContentType = bodyPart.getDataHandler.getContentType
+            val partContent = bodyPart.getContent
+            val expandedContent = {
+              partContent match {
+                case partMimeMessage: MimeMessage =>
+                  content(partMimeMessage)
+                case other =>
+                  other.toString
+              }
+            }
+            ("""
+              |[%s]
+              |%s
+            """.stripMargin).format(partContentType, expandedContent)
+          }
+        } mkString "\n"
+        case other =>
+          other.toString
+      }
+    }
+
+    ("""
+       |%s
+       |%s
+     """.stripMargin).format(headerBuilder.toString, content(mimeMessage))
+  }
+
 }
