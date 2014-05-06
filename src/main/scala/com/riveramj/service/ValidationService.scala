35c1fc4a0e18facfe3d0ee597ca30b5493d6ce6d
Mike Rivera <rivera.mj@gmail.com>
2014-05-06 15:05:34 -0400

    add valid email check to signup

--- src/main/scala/com/riveramj/service/ValidationService.scala
+++ src/main/scala/com/riveramj/service/ValidationService.scala
@@ -1,18 +1,21 @@
 package com.riveramj.service
 
 import com.riveramj.service.SurveyorService._
+
 import net.liftweb.common._
 import net.liftweb.http.S
 import net.liftweb.util.Props
+import net.liftweb.util.Helpers._
 
 object ValidationService extends Loggable {
+  val emailRegex = """^([^@]+)@([^@]+\.([^@].?)+)$""".r
 
   def checkEmpty(fieldValue: Option[String], fieldId: String): Box[ValidationError] = {
     checkEmpty(fieldValue getOrElse "", fieldId)
   }
 
   def checkEmpty(fieldValue: String, fieldId: String): Box[ValidationError] = {
-    if (fieldValue.trim.nonEmpty) {
+    if (fieldValue.nonEmpty) {
       Empty
     } else {
       Full(ValidationError(fieldId, S ? "Field Required"))
@@ -28,9 +31,28 @@ object ValidationService extends Loggable {
     } else Empty
   }
 
+  def validEmailFormat(email: String, errorId: String): Box[ValidationError] = {
+    val badEmail = Full(ValidationError(errorId, S ? "Not valid email address"))
+    if (email.nonEmpty) {
+      emailRegex.findFirstIn(email.trim) map {
+        _ => {
+          //  check by trying to use it
+          tryo {
+            val address = new javax.mail.internet.InternetAddress(email)
+            address.validate
+            Empty
+          } openOr badEmail
+        }
+      } getOrElse {
+        badEmail
+      }
+    } else Full(ValidationError(errorId, S ? "Field Required"))
+  }
+
   def checkEmail(email: String, errorId: String): Box[ValidationError] = {
     checkDuplicateEmail(email, errorId) or
-    checkEmpty(email, errorId)
+    checkEmpty(email, errorId) or
+    validEmailFormat(email, errorId)
   }
 
   def checkValidNumber(phoneNumber: String, errorId: String): Box[ValidationError] = {
